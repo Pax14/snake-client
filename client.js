dcc272af701743faea150c87d9a49385c063ed48
Paxon Cheung <paxon.cheung@gmail.com>
2021-01-16 05:04:46 +0000

    updating snake game

--- client.js
+++ client.js
@@ -1,16 +1,17 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: PC');
+    conn.write('Name: Sab');
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
--- input.js
+++ input.js
@@ -1,10 +1,8 @@
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = function() {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,6 +10,24 @@ const setupInput = function() {
   stdin.on('data', handleUserInput);
   return stdin;
 }
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+  if(data === 'w'){
+    connection.write('Move: up')
+  }
+  if(data === 's'){
+    connection.write('Move: down')
+  }
+  if(data === 'a'){
+    connection.write('Move: left')
+  }
+  if(data === 'd'){
+    connection.write('Move: right')
+  }
+};
 
 
 module.exports = {
